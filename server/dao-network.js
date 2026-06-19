3735dfae4b40864f4ba95e56701a4d1c432be198
kimi530 <55063258+kimi530@users.noreply.github.com>
2026-06-19 21:56:18 +0200

    some fix

--- server/dao-network.js
+++ server/dao-network.js
@@ -17,30 +17,28 @@ export const getFullNetwork = () => {
 export const getSegments = () => {
   return new Promise((resolve, reject) => {
     const query = `
-       -- forward segments
-       SELECT s1.id as from_id, s1.name as from_station,
-         s2.id as to_id, s2.name as to_station,
-         l.id as line_id, l.name as line_name
-       FROM line_stations ls1
-       JOIN line_stations ls2 ON ls1.line_id = ls2.line_id 
-          AND ls2.position = ls1.position + 1
-       JOIN stations s1 ON ls1.station_id = s1.id
-       JOIN stations s2 ON ls2.station_id = s2.id
-       JOIN lines l ON ls1.line_id = l.id
-            
-       UNION ALL
-
-       -- reverse segments (make each connection two-way)
-       SELECT s2.id as from_id, s2.name as from_station,
-         s1.id as to_id, s1.name as to_station,
-         l.id as line_id, l.name as line_name
-       FROM line_stations ls1
-       JOIN line_stations ls2 ON ls1.line_id = ls2.line_id 
-          AND ls2.position = ls1.position + 1
-       JOIN stations s1 ON ls1.station_id = s1.id
-       JOIN stations s2 ON ls2.station_id = s2.id
-       JOIN lines l ON ls1.line_id = l.id
-   `;
+      SELECT s1.id as from_id, s1.name as from_station,
+             s2.id as to_id, s2.name as to_station,
+             l.id as line_id, l.name as line_name
+      FROM line_stations ls1
+      JOIN line_stations ls2 ON ls1.line_id = ls2.line_id 
+                             AND ls2.position = ls1.position + 1
+      JOIN stations s1 ON ls1.station_id = s1.id
+      JOIN stations s2 ON ls2.station_id = s2.id
+      JOIN lines l ON ls1.line_id = l.id
+      
+      UNION ALL
+      
+      SELECT s2.id as from_id, s2.name as from_station,
+             s1.id as to_id, s1.name as to_station,
+             l.id as line_id, l.name as line_name
+      FROM line_stations ls1
+      JOIN line_stations ls2 ON ls1.line_id = ls2.line_id 
+                             AND ls2.position = ls1.position + 1
+      JOIN stations s1 ON ls1.station_id = s1.id
+      JOIN stations s2 ON ls2.station_id = s2.id
+      JOIN lines l ON ls1.line_id = l.id
+    `;
     db.all(query, [], (err, rows) => {
       if (err) reject(err);
       else resolve(rows);
